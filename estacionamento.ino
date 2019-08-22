f97b2845a8b6ee39937279ec1bf5aaced3155207
Valdir de Souza Junior <vsj@ic.ufal.br>
2019-08-22 00:32:35 -0300

    some changes
    Signed-off-by: Valdir de Souza Junior <vsj@ic.ufal.br>

--- estacionamento.ino
+++ estacionamento.ino
@@ -4,9 +4,10 @@
 #define vaga_0_1 1
 #define vaga_0_2 2
 #define vaga_0_3 3
+#define catraca_0 4
+#define catraca_1 5
 #define pinServo_0 2
 #define pinServo_1 3
-
 
 //TODO: Adicionar delay entre cada leitura.
 //LiquidCrystal lcd(<pino RS>, <pino enable>, <pino D4>, <pino D5>, <pino D6>, <pino D7>)
@@ -16,47 +17,55 @@ Servo servo0;
 Servo servo1;
 String string1 = "";
 String string2 = "";
-int pinCatracas[] = {A8, A9};
-int pinVagas[] = { A0, A1, A2, A3};
-int stateVagas[] = { 0, 0, 0, 0};
-int pinLedVagas[] = {4, 5, 6, 7};
-int ociosoVagas[] = {0, 0, 0, 0};
-int ociosoCatracas[] = {0, 0};
+bool timer0=0, timer1=0;
+int countTimer0, countTimer1;
+int lastReading[] = {0,0,0,0,0,0};
+int pinSensores[] = { A0, A1, A2, A3, A8, A9};
+int stateVagas[] = { 0, 0, 0, 0}; 
+int pinLedVagasVerde[] = {4, 5, 6, 7};
+int pinLedVagasVermelho[] = {8, 9, 10, 11};
+int ocioso[] = {0, 0, 0, 0,0,0};
 int stateCatracas[] = {0, 0};
 int contadorCatracas[] = {0, 0};
 int vagas = 4;
 
-void checkVaga(int pos) {
+void checkSensores() {
 
-  int val = 0;
-  for (int i = 0; i < 10; i++) {
-    val += analogRead(pinVagas[pos]);
+  for (int i = 0; i < 10 ; i++){
+      for (int j = 0; j < 6; j++) {
+        if (i ==0)
+        {
+          lastReading[j] =0;
+        }
+        lastReading[j] += analogRead(pinSensores[j]);
+      }
   }
-  val = val / 10;
+  for (int i = 0; i < 6; i++) {
+     lastReading[i] = lastReading[i] / 10;
+    
+  }
+/*
   if (true) { //TODO: Range de aceitação.
     stateVagas[pos] = 1;
   }
   else {
     stateVagas[pos] = 0 ;
   }
-
+*/
 
 }
 
-void checkVagas() {
-  vagas = 0;
-  for (int i = vaga_0_0 ; i <= vaga_0_3; i++) {
-    checkVaga(i);
-  }
-}
+
 void showVagas() {
   int temp;
   for (int i = vaga_0_0 ; i <= vaga_0_3; i++) {
     if (stateVagas[i] == 1) {
-      digitalWrite(pinLedVagas[i], HIGH);
+      digitalWrite(pinLedVagasVermelho[i], HIGH);
+      digitalWrite(pinLedVagasVerde[i], LOW);
     }
     else {
-      digitalWrite(pinLedVagas[i], LOW);
+      digitalWrite(pinLedVagasVermelho[i], LOW);
+      digitalWrite(pinLedVagasVerde[i], HIGH);
     }
   }
 }
@@ -64,31 +73,38 @@ void showVagas() {
 
 void checkCatracas() {
 
-  int val;
   // catraca 0 ENTRADA
   if (stateCatracas[0] == 0) { // Se estiver fechada.
-    val = 0;
-    for (int i = 0; i < 10; i++) {
-      val += analogRead(pinCatracas[0]);
-    }
-    val = val / 10;
     if ((true) && (vagas > 0)) {
-      // TODO: Abre catraca por 10 segundos.
+      timer0=true;
+      countTimer0 = millis();
       servo0.write(90);
       vagas --;
     }
   }
+  else if (timer0==true){
+    if (millis()-countTimer0 >=10000)
+    {
+      servo1.write(0);
+      timer0=false;
+    }
+  }
+
+  
   // catraca 1 SAÍDA
   if (stateCatracas[1] == 0) { // Se estiver fechada.
-    val = 0;
-    for (int i = 0; i < 10; i++) {
-      val += analogRead(pinCatracas[1]);
-    }
-    val = val / 10;
     if ((true) && (vagas > 0)) {
-      // TODO: Abre catraca por 10 segundos.
+      timer1=true;
+      countTimer1 = millis();
       servo1.write(90);
       vagas++ ;
+    }
+  }
+    else if (timer1==true){
+    if (millis()-countTimer1 >=10000)
+    {
+      servo1.write(0);
+      timer1=false;
     }
   }
 
@@ -104,35 +120,26 @@ void setup() {
   servo0.write(0); //TODO: Conferir se 0 graus é fechado.
   servo1.write(0);
   for (int i = vaga_0_0; i <= vaga_0_3; i++ ) {
-    pinMode(pinLedVagas[i], OUTPUT);
+    pinMode(pinLedVagasVerde[i], OUTPUT);
+    pinMode(pinLedVagasVermelho[i], OUTPUT);
   }
-
-  // Leitura do Sensor das Catracas Ocioso.
+  checkSensores();
+  for(int i =0; i<6; i++){
+    ocioso[i] = lastReading[i];
+  }
   int temp;
-  for (int i = 0; i < 2; i++) {
-    temp = 0;
-    for (int j = 0; j < 10; j++) {
-      temp += analogRead(pinCatracas[i]);
-
-    }
-    ociosoCatracas[i] = temp / 10;
-  }
-
-  // Leitura do Sensor das Vagas
-  for (int i = 0; i < 4; i++) {
-    temp = 0;
-    for (int j = 0; j < 10; j++) {
-      temp += analogRead(pinVagas[i]);
-    }
-    ociosoVagas[i] = temp / 10;
-  }
+  
 
 }
 
+
 void loop() {
-  checkVagas();
+  checkSensores();
   showVagas();
   checkCatracas();
+  string1 = "Ha ";
+  string2 = string1 + vagas;
+  string1= string2 +" vagas.";
   //TODO: Atualiza string1 para  "Ha x vagas" se houver vagas, senão "Não ha vagas."
   lcd.clear();
   lcd.print(string1);
